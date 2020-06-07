63ee211e1b8c99f967633ba2e6e8ce6225fb4f93
Paul Touchton <p_touchton@yahoo.com>
2020-06-06 18:19:57 -0700

    adding animation

--- APM/src/app/app.module.ts
+++ APM/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 
 // Imports for loading & configuring the in-memory web api
@@ -24,7 +25,8 @@ import { AppRoutingModule } from './app-routing.module';
     ProductModule,
     UserModule,
     MessageModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   declarations: [
     AppComponent,
